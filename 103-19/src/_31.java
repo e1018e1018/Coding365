bd002ea102d287cd2db003e8d0a2b8906c9e136c
e1018e1018 <e1018e1018@gmail.com>
2018-07-21 14:17:58 +0800

    2018/07/21

--- 103-19/src/_31.java
+++ 103-19/src/_31.java
@@ -13,7 +13,7 @@ public class _31 {
 		String article = input.nextLine();
 		String word_1 = input.next();
 		String word_2 = input.next();		
-		String replace = article.replace(" " + word_1 + " ", " " + word_2 + " ");		
+		String replace = "";		
 		String[] split = article.split(" ");
 		ArrayList<String> array_1 = new ArrayList<String>();
 		ArrayList<String> array_2 = new ArrayList<String>();
@@ -21,6 +21,13 @@ public class _31 {
 		ArrayList <String> list  = new ArrayList<String>();
 		String add = "";
 		String remove = "";
+		for(int i = 0; i < split.length; i++) {
+			if(split[i].equals(word_1)) {
+				replace += word_2 + " ";
+			}else {
+				replace += split[i] + " ";
+			}
+		}
 		for(int i = 0; i < split.length; i++) {
 			if(split[i].equals(word_1)) {
 				array_1.add(word_2 + " ");
@@ -34,9 +41,18 @@ public class _31 {
 		for(int m = 0; m < split.length; m++) {
 			array_2.add(split[m] + " ");
 		}
-		for(int n = 0; n < array_2.size(); n++) {
-			array_2.remove(word_1 + " ");
-			remove += array_2.get(n);
+		int size = array_2.size();
+		for(int n = 0; n < size; n++) {
+			while(array_2.get(n).equals(word_1 + " ")) {
+				array_2.remove(n);
+				size--;
+				if(size == n) {
+					break;
+				}
+			}
+			if(size != n) {
+				remove += array_2.get(n);
+			}
 		}
 
 		for(int x = 0; x < split.length; x++) {
@@ -52,19 +68,29 @@ public class _31 {
 	               map.put(split[x], 1);  
 	           } 
 		}
+		for (Object key : map.keySet()) {
+			list.add(map.get(key) + " " + key);
+		}
+		Collections.sort(list);
 
+		for(int i = 0; i < list.size(); i++) {
+			String[] split_0 = list.get(i).split(" ");
+			if(Integer.parseInt(list.get(list.size() - 1).substring(0, 1)) > Integer.parseInt(list.get(i).substring(0, 1))) {
+				String temp = list.get(list.size() - 1);
+				for(int j = list.size() - 1; j > 0 ; j--) {
+					list.set(j, list.get(j - 1));
+				}
+				list.set(0, temp);
+			}
+		}
+		
 		System.out.println(replace);
 		System.out.println(add);
 		System.out.println(remove);
-		
-		for (Object key : map.keySet()) {
-			list.add(key + " " + map.get(key));
+		for(int k = 0; k < list.size(); k++) {
+			String[] split_1 = list.get(k).split(" ");
+			list.set(k, split_1[1] + " " + split_1[0]);
+			System.out.println(list.get(k));
 		}
-		
-		Collections.sort(list);
-		System.out.println(list);
-
-
 	}
-
 }
