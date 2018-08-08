cc25d09fc82589a0978a8a3d4c6c1668c425a926
e1018e1018 <e1018e1018@gmail.com>
2018-08-09 00:40:36 +0800

    2018/08/09

--- 104-05/src/_65.java
+++ 104-05/src/_65.java
@@ -1,9 +1,30 @@
-
+import java.util.*;
 public class _65 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		
+		int num = input.nextInt();
+		while(num > 0) {
+			String data = input.nextLine();
+			String[] split = data.split(" ");
+			University.schoolName = split[0];
+			for(int i = 1; i < split.length; i++) {
+				University.attribute += split[i] + ",";
+			}
+			num--;
+		}
 	}
 
+}
+
+class University{
+	HashMap<String, String> school = new HashMap<String, String>();
+	static String schoolName;
+	static String attribute;
+	
+	University() {
+		school.put(schoolName, attribute);
+	}
 }
--- 104-10/src/_70.java
+++ 104-10/src/_70.java
@@ -41,3 +41,4 @@ class Class{
 		return 0;
 	}
 }
+
